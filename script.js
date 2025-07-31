ba9db654788146c47106e5c3659cb85afc3a37c9
teserportfolio <tilleser@hotmail.de>
2025-07-31 13:48:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-const header = document.querySelector('header');
-const menuToggle = document.getElementById('menuToggle');
+document.addEventListener('DOMContentLoaded', function() {
+  const header = document.getElementById('header');
+  const menuToggle = document.getElementById('menu-toggle');
 
-window.addEventListener('scroll', () => {
-  if (window.scrollY > 10) {
-    header.classList.add('scrolled');
-  } else {
-    header.classList.remove('scrolled');
-  }
-});
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
+  });
 
-menuToggle.addEventListener('click', () => {
-  menuToggle.classList.toggle('active');
+  menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active');
+  });
 });
